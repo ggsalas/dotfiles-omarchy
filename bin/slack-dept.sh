731155cc6c677e9643be907f52b4e5536cc911c2
Gabriel German Salas <ggsalas@gmail.com>
2025-09-21 23:37:50 -0300

    Updates for omarchy3

---
+++ bin/slack-dept.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+exec chromium --user-data-dir=/home/gabriel/.config/chromium \
+              --profile-directory="Profile 3" \
+              --app=https://app.slack.com/client/E026KL2NWPK/
+
